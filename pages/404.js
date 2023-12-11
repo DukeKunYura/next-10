33f2dd2d543684c35dfb13fdab3085f704abd956
dukekunyura <dukekunyura@gmail.com>
2023-12-12 02:10:39 +0300

    [+] add build script

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Link from 'next/link';
+import MainContainer from "../components/MainContainer";
+
+const NotFound = () => {
+  return (
+    <MainContainer keywords={"users"}>
+      Not Found !!
+    </MainContainer>
+  )
+}
+
+export default NotFound;
