bacea45e34621a9eeb18e842ce31eeceb32a2124
dukekunyura <dukekunyura@gmail.com>
2023-12-12 01:36:03 +0300

    [+] add getServerSideProps

--- pages/user/[id].js
+++ pages/user/[id].js
@@ -1,12 +1,27 @@
 import {useRouter} from 'next/router';
 import styles from '../../styles/User.module.scss';
 
-export default function User() {
+export default function User({user}) {
     const {query} = useRouter();
+    console.log(user)
     return (
         <div className={styles.user}>
             <h3>User id: {query.id}</h3>
+            <h3>{user.name}</h3>
 
         </div>
     )
 }
+
+export async function getServerSideProps({params}) {
+
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`)
+    const user = await res.json()
+    console.log(user)
+  
+    return {
+      props: {
+        user
+      }
+    }
+  }
