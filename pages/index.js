ce10850e4daf61754009663d5a1d9f80253ac339
dukekunyura <dukekunyura@gmail.com>
2023-12-12 01:48:28 +0300

    [+] add Head

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
+import Head from 'next/head';
 import ALink from '../components/A';
 
 const Index = () => {
   return (
-    <div>
+    <>
+      <Head>
+        <meta keywords="next, ssr, test"></meta>
+        <title>General</title>
+      </Head>
+      <div>
         <div className='navbar'>
-            <ALink href={'/'} text={'General'}/>
-            <ALink href={'/users'} text={'Users'}/>
+          <ALink href={'/'} text={'General'}/>
+          <ALink href={'/users'} text={'Users'}/>
         </div>
-    </div>
+      </div>
+    </>
+    
   )
 }
 
